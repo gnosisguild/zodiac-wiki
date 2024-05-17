31b1caa2c763f602ea57653403323f898af23aaa
samepant <samepant@users.noreply.github.com>
2024-05-17 13:58:52 -0400

    page setup, config mods

--- theme.config.jsx
+++ theme.config.jsx
@@ -3,5 +3,12 @@ export default {
   project: {
     link: "https://github.com/gnosisguild/zodiac-wiki",
   },
+  docsRepositoryBase: "https://github.com/gnosisguild/zodiac-wiki",
+  darkMode: false,
+  nextThemes: {
+    defaultTheme: "dark",
+  },
+  primaryHue: 45,
+  primarySaturation: 30,
   // ... other theme options
 };
