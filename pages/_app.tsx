b2471f47f97d940c9f25f351a7a71ece102c3641
samepant <samepant@users.noreply.github.com>
2024-05-24 16:33:12 -0400

    adjust _app file

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default App;
