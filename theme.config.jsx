cf20778bbf22c0fb74ca085f3437cc55e9703f20
samepant <samepant@users.noreply.github.com>
2024-07-12 12:08:26 -0400

    add meta info to nextra theme

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,3 +1,5 @@
+import { useConfig } from "nextra-theme-docs";
+
 export default {
   logo: <span>Zodiac Wiki</span>,
   project: {
@@ -12,6 +14,43 @@ export default {
   primarySaturation: 30,
   sidebar: {
     defaultMenuCollapseLevel: 1,
+  },
+  head: function useHead() {
+    const { title } = useConfig();
+    return (
+      <>
+        <title>Zodiac Wiki</title>
+        <meta name="title" content={title ? title : "Zodiac Wiki"} />
+        <meta
+          name="description"
+          content="An open, living resource for DAOs and documentation of the Zodiac standard."
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.zodiac.wiki/" />
+        <meta property="og:title" content="Zodiac Wiki" />
+        <meta
+          property="og:description"
+          content="An open, living resource for DAOs and documentation of the Zodiac standard."
+        />
+        <meta
+          property="og:image"
+          content="https://www.zodiac.wiki/meta_img.jpg"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.zodiac.wiki/" />
+        <meta property="twitter:title" content="Zodiac Wiki" />
+        <meta
+          property="twitter:description"
+          content="An open, living resource for DAOs and documentation of the Zodiac standard."
+        />
+        <meta
+          property="twitter:image"
+          content="https://www.zodiac.wiki/meta_img.jpg"
+        />
+      </>
+    );
   },
   // ... other theme options
 };
